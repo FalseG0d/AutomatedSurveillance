2edaa4d25c98157a88cb8409dcfdc633012960f0
Apoorv Garg <u.garg.10@gmail.com>
2020-04-19 17:03:57 +0530

    Errors fixed

--- network.py
+++ network.py
@@ -1,10 +1,11 @@
 import socket
+import pickle
 
 class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048))
 
         except:
             pass
@@ -14,15 +15,14 @@ class Network:
         self.server="192.168.1.2"
         self.port=5555
         self.addr=(self.server,self.port)
-        self.pos=self.connect()
+        self.p=self.connect()
         
-    def getPos(self):
-        return self.pos
-        
+    def getP(self):
+        return self.p
+
     def send(self,data):
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
-
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
